2cac391fe0286a7eeccf7a84456482807d40daff
anand10062000 <anandkumarramisetti@gmail.com>
2021-09-17 17:12:58 +0530

    wage for full day

---
+++ Fulldaywage.java
@@ -0,0 +1,23 @@
+public class Fulldaywage {
+
+	public static void main(String[] args) {
+                int fullday=8;
+                int parttime=8;
+                int wage=20;
+		Random ran = new Random();
+		int n=ran.nextInt(3);
+		System.out.println(n);
+		if(n==0) 
+			System.out.println("obsent");
+		else if(n==1)
+			System.out.println("present for full time");
+                        int wageforday=wage*fulltime;
+                        System.out.println("wage is"+wageforday);
+
+		else
+			System.out.print("part time employee");
+		// TODO Auto-generated method stub
+
+	}
+
+}
